b8b3b8abc6c645f7fc848e009cd90b9a1d733444
tran pham <trannpham.05@gmail.com>
2023-09-21 10:42:46 -0500

    did everything (i win)

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Stack<Integer> nums = new Stack<Integer>();
+        /*Stack<Integer> nums = new Stack<Integer>();
 
         nums.push(10);
         nums.push(9);
@@ -13,5 +13,48 @@ public class App {
         System.out.println(nums.isEmpty());
         nums.pop();
         System.out.println(nums.isEmpty());
+        */
+
+
+        System.out.println(postFix("67+"));
+        System.out.println(postFix("98*"));
+        
+    }
+
+    public static int postFix(String operation)
+    {
+        Stack<String> ops = new Stack<String>();
+        
+        for(int i = 0; i < operation.length(); i++)
+        {
+            String top = operation.substring(i, i+1);
+            if(top.equals("+"))
+            {
+                int x = Integer.parseInt(ops.pop());
+                int y = Integer.parseInt(ops.pop());
+                ops.push(Integer.toString(add(x, y)));
+            }
+            else if(top.equals("*"))
+            {
+                int x = Integer.parseInt(ops.pop());
+                int y = Integer.parseInt(ops.pop());
+                ops.push(Integer.toString(multiply(x, y)));
+            }
+            else
+            {
+                ops.push(top);
+            }
+        }
+        return Integer.parseInt(ops.pop());
+    }
+
+    public static int add(int X, int Y)
+    {
+        return X + Y;
+    }
+
+    public static int multiply(int X, int Y)
+    {
+        return X * Y;
     }
 }
